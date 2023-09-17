9f32cca3ca4105c395dabceb632aa160ec146249
karthik <karthikssalian5@gmail.com>
2023-09-17 14:56:07 +0530

    updated vite config to adjust gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+// base ia added just for gh-pages to work remove is gh-pages is not used
 export default defineConfig({
   plugins: [react()],
+  base: "/kannada-quiz-site/"
 })
