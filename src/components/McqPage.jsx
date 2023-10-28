17485d910d87ddc712a7003d6039f9c75e4c035b
Karthik S Salian <karthikssalian5@gmail.com>
2023-10-28 10:40:46 +0530

    corrected wrong question paper

--- src/components/McqPage.jsx
+++ src/components/McqPage.jsx
@@ -1,19 +1,10 @@
-import samskrutikaMse1 from "../data/samskruthika_kannada_mse_1.json"
-import samskrutikaMse2 from "../data/samskruthika_kannada_mse_2.json"
-import balakeMse1 from "../data/balake_kannada_mse_1.json"
-import balakeMse2 from "../data/balake_kannada_mse_1.json"
 import Question from "./Question"
 import "./mcq.css"
-import { questionSets } from './../constants'
+import { questionSets ,getData} from './../constants'
 
 function McqPage({ questionSet, attemptType }) {
-    let data = {}
-    if(questionSet==questionSets[0]){
-        data = (questionSet == questionSets[0] )?samskrutikaMse1:balakeMse1;
-    }else{
-        data = (questionSet == questionSets[0]) ?samskrutikaMse2:balakeMse2;
-    }
-
+    console.log(questionSet,attemptType)
+    const data = getData(questionSet);
     return (
         <>
             <ul className="question-list-container kannada-font">
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,19 @@
+import samskrutikaMse1 from "./data/samskruthika_kannada_mse_1.json"
+import samskrutikaMse2 from "./data/samskruthika_kannada_mse_2.json"
+import balakeMse1 from "./data/balake_kannada_mse_1.json"
+import balakeMse2 from "./data/balake_kannada_mse_2.json"
+
 export const attemptTypes = ["learner", "intermediate"]
 export const questionSets = ["balake kannada mse-1", "samskrutika kannada mse-1","balake kannada mse-2", "samskrutika kannada mse-2"]
+
+export function getData(questionSet){
+    if(questionSet==questionSets[0])
+        return balakeMse1;
+    if(questionSet==questionSets[1])
+        return samskrutikaMse1
+    if(questionSet==questionSets[2])
+        return balakeMse2
+    if(questionSet==questionSets[3])
+        return samskrutikaMse2
+    return [];
+}
